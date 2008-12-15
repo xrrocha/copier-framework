8e9a1042c75e54fb4a008de31ce19234d47d821c
xrrocha <xrrocha@5c81f538-c892-11dd-a412-a1792178251d>
2008-12-15 20:28:59 +0000

    Corrected Yaml config repetitive configuration. Added simple log formater

--- trunk/src/main/plenix/copier/Main.java
+++ trunk/src/main/plenix/copier/Main.java
@@ -4,19 +4,18 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import net.sourceforge.yamlbeans.YamlConfig;
 import net.sourceforge.yamlbeans.YamlReader;
+import plenix.copier.Copier;
 
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getSimpleName());
     
-    private static final Map<String, Class<?>> classTagMap = new HashMap<String, Class<?>>();
-    private static final Map<Class<?>, Map<String, Class<?>>> propertyMap = new HashMap<Class<?>, Map<String, Class<?>>>();
-    
+    private static final YamlConfig config = new YamlConfig();
     static {
         try {
             Enumeration<URL> locations = Main.class.getClassLoader().getResources("recordCopierConfig.yaml");
@@ -24,37 +23,36 @@ public class Main {
                 URL location = locations.nextElement();
                 YamlReader reader = new YamlReader(new InputStreamReader(location.openStream()));
 
-                if (logger.isLoggable(Level.FINE)) logger.fine("INIT: Loading tags from '" + location + "'");
                 @SuppressWarnings("unchecked")
                 Map<String, String> tagMap = (Map<String, String>) reader.read();
                 if (tagMap != null) {
+                    if (logger.isLoggable(Level.FINE)) logger.fine("Loading tags from '" + location + "'");
                     for (String tagName: tagMap.keySet()) {
                         String className = tagMap.get(tagName);
                         try {
                             Class<?> clazz = Class.forName(className);
-                            if (logger.isLoggable(Level.FINE)) logger.fine("INIT: Tag '" + tagName + "' maps to class '" + clazz.getName() + "'");
-                            classTagMap.put(tagName, clazz);
+                            config.setClassTag(tagName, clazz);
+                            if (logger.isLoggable(Level.FINE)) logger.fine("Tag '" + tagName + "' maps to class '" + clazz.getName() + "'");
                         } catch (Exception e) {
                             logger.severe("Error loading class '" + className + "': " + e);
                         }
                     }
                 }
                 
-                if (logger.isLoggable(Level.FINE)) logger.fine("INIT: Loading property element types from '" + location + "'");
                 @SuppressWarnings("unchecked")
                 Map<String, Map<String, String>> classMap = (Map<String, Map<String, String>>) reader.read();
                 if (classMap != null) {
+                    if (logger.isLoggable(Level.FINE)) logger.fine("Loading property element types from '" + location + "'");
                     for (String className: classMap.keySet()) {
                         try {
                             Class<?> targetClass = Class.forName(className);
                             Map<String, String> propertyNameMap  = classMap.get(className);
-                            Map<String, Class<?>> propertyClassMap = new HashMap<String, Class<?>>();
                             for (String propertyName: propertyNameMap.keySet()) {
                                 String propertyClassName = propertyNameMap.get(propertyName);
-                                Class<?> propertyClass = Class.forName(propertyClassName);
-                                propertyClassMap.put(propertyName, propertyClass);
+                                Class<?> propertyType = Class.forName(propertyClassName);
+                                config.setPropertyElementType(targetClass, propertyName, propertyType);
+                                if (logger.isLoggable(Level.FINE)) logger.fine("Property '" + propertyName + "' of class '" + className + "' is of type '" + propertyType + "'");
                             }
-                            propertyMap.put(targetClass, propertyClassMap);
                         } catch (Exception e) {
                             logger.severe("Error setting property type for '" + className + "': " + e);
                         }
@@ -68,18 +66,9 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         String yamlFilename = args[0];
-        
-        YamlReader reader = new YamlReader(new FileReader(yamlFilename));
-        for (String tagName: classTagMap.keySet()) {
-            reader.getConfig().setClassTag(tagName, classTagMap.get(tagName));
-        }
-        for (Class<?> targetClass: propertyMap.keySet()) {
-            Map<String, Class<?>> propertyNameMap = propertyMap.get(targetClass);
-            for (String propertyName: propertyNameMap.keySet()) {
-                Class<?> propertyClass = propertyNameMap.get(propertyName);
-                reader.getConfig().setPropertyElementType(targetClass, propertyName, propertyClass);
-            }
-        }
+        logger.info("Copier: processing config file: " + yamlFilename);
+
+        YamlReader reader = new YamlReader(new FileReader(yamlFilename), config);
 
         Copier<?> copier = reader.read(Copier.class);
         
---
+++ trunk/src/main/plenix/util/logging/BasicFormatter.java
@@ -0,0 +1,20 @@
+package plenix.util.logging;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class BasicFormatter extends Formatter {
+    @Override
+    public String format(LogRecord record) {
+        final StringBuilder builder = new StringBuilder();
+        String className = record.getSourceClassName();
+        className = className.substring(className.lastIndexOf('.') + 1);
+        builder.append(className);
+        builder.append('.');
+        builder.append(record.getSourceMethodName());
+        builder.append(": ");
+        builder.append(record.getMessage());
+        builder.append('\n');
+        return builder.toString();
+    }
+}
